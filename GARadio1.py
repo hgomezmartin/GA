8099516542da5f9a1dbd7607ddac74827f56d289
hgomezmartin <hugogmartin@hotmail.com>
2022-10-19 01:36:40 +0200

    selectAlbumIfAndImputLowercase

--- GARadio1.py
+++ GARadio1.py
@@ -1,35 +1,37 @@
 from pygame import mixer
 
-mixer.init()
 
-album=str(input("Selecciona el album:\n 1. Zaba\n 2. How To Be a Human Being\n 3. Dreamland \n"))
-    #añadimos la ruta
+chooseAlbum = str(input("Selecciona el album:\n 1. Zaba\n 2. How To Be a Human Being\n 3. Dreamland \n"))
+chooseAlbum = chooseAlbum.lower()
+if chooseAlbum == "zaba":
+        album = 'zaba.mp3'
+
+elif chooseAlbum == "htbahb":
+        album = 'How_To_Be_A_Human_Being.mp3'
+
+elif chooseAlbum == "dreamland":
+        album = 'dreamland.mp3'
+
+
+mixer.init()
 mixer.music.load(album)
 mixer.music.set_volume(0.7)
 mixer.music.play()
 
 
-
-
 while True:
     print("Pulsa p para detener reproduccón")
     print("Pulsa r para reanudar reproducción")
-    print("pulsa e para elegir album")
+    #print("pulsa e para elegir album")
     print("Pulse s para salir")
-    
+
     opcion = input(">")
-    
+
     if opcion == "p":
         mixer.music.pause()
     elif opcion == "r":
         mixer.music.unpause()
-    elif opcion == "e":
-        mixer.musc.stop()
-        album=str(input("Selecciona el album:\n 1. Zaba\n 2. How To Be a Human Being\n 3.Dreamland"))
-        mixer.music.load(album)
-        mixer.music.set_volume(0.7)
-        mixer.music.play()
+   # elif opcion == "e":
+    #    mixer.music.stop()
     elif opcion == "s":
         mixer.music.stop()
- 
-        
